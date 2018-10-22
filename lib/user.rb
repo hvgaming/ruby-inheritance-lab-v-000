dd39d939dd1d617f219c8edecd7d6f8d65d18a40
hugo valois <hvgaming@yahoo.com>
2018-10-22 21:57:04 +0000

    automatically backed up by learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,6 +1,6 @@
 class User
 
-attr_accessor :first_name, :last_name
+attr_accessor :first_name = nil, :last_name = nil
    def initialize(first_name, last_name)
     @first_name = first_name
     @last_name = last_name
